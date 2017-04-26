90b0530824074d413377aee8aa311b9c17bebc7d
Tyler Lugger <tyler.lugger@colorado.edu>
2017-04-26 09:18:29 -0600

    Fix tabs in test script

--- testBankMining.py
+++ testBankMining.py
@@ -3,8 +3,8 @@ from BankMining import rowCount
 
 class TestFileSize(unittest.TestCase):
     def test_size(self):
-	filesize = 614
-	self.assertEqual(filesize, rowCount())
+        filesize = 614
+        self.assertEqual(filesize, rowCount())
 
 if __name__ == '__main__':
     unittest.main()
