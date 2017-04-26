154e105facba935a3d96de16e0d1c6049d12e524
Tyler Lugger <tyler.lugger@colorado.edu>
2017-04-26 09:11:36 -0600

    Added a unit test for the converted script

---
+++ testBankMining.py
@@ -0,0 +1,10 @@
+import unittest
+from BankMining import rowCount
+
+class TestFileSize(unittest.TestCase):
+    def test_size(self):
+	filesize = 614;
+	self.assertEqual(filesize, rowCount())
+
+if __name__ == '__main__':
+    unittest.main()
